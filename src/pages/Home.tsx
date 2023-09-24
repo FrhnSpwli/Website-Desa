0aa8dc972813a2dfe42dc06b69ecabadf6b8a375
FrhnSpwli <96256195+FrhnSpwli@users.noreply.github.com>
2023-09-24 18:08:55 +0800

    update IonToolbar

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home: React.FC = () => {
     <IonPage>
       <IonHeader>
         <IonToolbar>
-          <IonTitle>Blank</IonTitle>
+          <IonTitle>Sistem Informasi 13</IonTitle>
         </IonToolbar>
       </IonHeader>
       <IonContent fullscreen>
