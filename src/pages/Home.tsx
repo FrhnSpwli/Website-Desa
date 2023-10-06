d2afa43b8104c8427ed3f2585142a7197010918d
FrhnSpwli <96256195+FrhnSpwli@users.noreply.github.com>
2023-10-06 14:44:34 +0800

    insert component to home page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,23 +1,15 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import ExploreContainer from '../components/ExploreContainer';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
 import './Home.css';
 
 const Home: React.FC = () => {
   return (
     <IonPage>
-      <IonHeader>
-        <IonToolbar>
-          <IonTitle>Sistem Informasi 13</IonTitle>
-        </IonToolbar>
-      </IonHeader>
-      <IonContent fullscreen>
-        <IonHeader collapse="condense">
-          <IonToolbar>
-            <IonTitle size="large">Blank</IonTitle>
-          </IonToolbar>
-        </IonHeader>
-        <ExploreContainer />
-      </IonContent>
+      <Header />
+      <ExploreContainer />
+      <Footer />
     </IonPage>
   );
 };
