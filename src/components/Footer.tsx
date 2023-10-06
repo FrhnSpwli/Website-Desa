5ae9f4d56852036d70f6dc3fd619a8149434d147
FrhnSpwli <96256195+FrhnSpwli@users.noreply.github.com>
2023-10-06 14:42:35 +0800

    add footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { IonContent, IonFooter, IonTitle, IonToolbar } from '@ionic/react';
+
+function Footer() {
+  return (
+    <>
+      <IonFooter>
+        <IonToolbar>
+          <IonTitle>Footer</IonTitle>
+        </IonToolbar>
+      </IonFooter>
+    </>
+  );
+}
+export default Footer;
