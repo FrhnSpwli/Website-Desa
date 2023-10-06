fa347ae1a379b78e077f2120996cdca78994a5a0
FrhnSpwli <96256195+FrhnSpwli@users.noreply.github.com>
2023-10-06 14:42:15 +0800

    add header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/react';
+
+function Header() {
+    return (
+        <>
+            <IonHeader>
+                <IonToolbar>
+                    <IonTitle>Header</IonTitle>
+                </IonToolbar>
+            </IonHeader>
+        </>
+    );
+}
+export default Header;
