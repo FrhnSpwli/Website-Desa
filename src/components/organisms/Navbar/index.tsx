793b2947e5db3135ff502a5e80b27cbea0fd8629
FrhnSpwli <96256195+FrhnSpwli@users.noreply.github.com>
2023-10-12 23:00:23 +0800

    fixing component Navbar

--- src/components/organisms/Navbar/index.tsx
+++ src/components/organisms/Navbar/index.tsx
@@ -1,35 +1,37 @@
-import React from 'react';
-import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar, IonSearchbar  } from '@ionic/react';
+import React, { Children, ReactNode } from 'react';
+import { IonButtons, IonContent, IonHeader, IonMenu, IonMenuButton, IonPage, IonTitle, IonToolbar, IonSearchbar } from '@ionic/react';
 import Button from '../../atoms/button';
 import Styles from './Navbar.module.css'
-function Example() {
-    return (
-      <>
-        <IonMenu side="end" contentId="main-content">
-          <IonHeader>
-            <IonToolbar>
-              <IonTitle>Menu Content</IonTitle>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent className="ion-padding">
-            <Button>Push me</Button>
-          </IonContent>
-        </IonMenu>
-        <IonPage id="main-content">
-          <IonHeader>
-            <IonToolbar>
-            <div className={Styles.menu}>
-              <IonTitle>About</IonTitle>
-              <IonSearchbar></IonSearchbar>
-            </div>
-              <IonButtons slot="end">
-                <IonMenuButton></IonMenuButton>
-              </IonButtons>
-            </IonToolbar>
-          </IonHeader>
-          <IonContent className="ion-padding">Tap the button in the toolbar to open the menu.</IonContent>
-        </IonPage>
-      </>
-    );
-  }
-  export default Example;
+
+interface NavbarProps {
+  children: ReactNode;
+}
+
+const Navbar = ({ children }: NavbarProps) => {
+  return (
+    <>
+      <IonMenu side="end" contentId="main-content">
+        <IonHeader>
+          <IonToolbar>
+            <IonTitle>Menu Content</IonTitle>
+          </IonToolbar>
+        </IonHeader>
+        <IonContent className="ion-padding">
+          <Button>Push me</Button>
+        </IonContent>
+      </IonMenu>
+      <IonHeader>
+        <IonToolbar>
+          <div className={Styles.menu}>
+            <IonTitle>{children}</IonTitle>
+            <IonSearchbar></IonSearchbar>
+          </div>
+          <IonButtons slot="end">
+            <IonMenuButton></IonMenuButton>
+          </IonButtons>
+        </IonToolbar>
+      </IonHeader>
+    </>
+  );
+}
+export default Navbar;
