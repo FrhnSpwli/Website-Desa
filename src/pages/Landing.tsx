3fd3cb6c359ae4d07172222ef41d3e2a26220827
syarifahb <syarifahb04@gmail.com>
2023-10-16 21:35:11 +0800

    update

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonMenu, IonMenuButton, IonButton, IonButtons, IonText } from '@ionic/react';
 import Footer from '../components/Footer';
 import Header from '../components/Header';
 import './Landing.css';
@@ -7,11 +7,22 @@ const Landing: React.FC = () => {
   return (
     <IonPage>
       <Header />
-      <div className='landing-container'>
-        <h1>Landing</h1>
-      </div>
-      <Footer />
+
+        <IonContent>
+          <div className="landing-content">
+           <section className='landing-content-button'> 
+           <IonText><h1 className='landing-content-heading1'>Welcome to My Landing Page</h1></IonText>
+            <IonText><p className='landing-content-heading2'>This is a simple landing page.</p></IonText>
+            <IonButton> 
+              Learn More 
+            </IonButton>
+            </section>
+            <img src='./Fontaine Genshin impact.jpg' alt="image"/>
+          </div>
+        </IonContent>
+        <Footer />
     </IonPage>
+    
   );
 };
 
