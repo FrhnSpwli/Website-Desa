65f1ca402372b15ec9099150a9d10c7700bd8d1d
FrhnSpwli <96256195+FrhnSpwli@users.noreply.github.com>
2023-10-25 02:35:08 +0800

    add styles for not-found page

--- src/pages/notFound.tsx
+++ src/pages/notFound.tsx
@@ -1,18 +1,24 @@
 import React from 'react';
-import { IonPage, IonContent } from '@ionic/react';
-import { RouteComponentProps, useHistory } from 'react-router';
+import { IonPage } from '@ionic/react';
+import { RouteComponentProps } from 'react-router';
 import Button from '../components/atoms/button';
+import Styles from '../styles/notFound.module.css';
 
 const NotFound: React.FC<RouteComponentProps> = ({ location }) => {
-    const history = useHistory();
 
     return (
         <IonPage>
-            <IonContent>
-                <h2>Sorry, we couldn't find {location.pathname}</h2>
-            </IonContent>
-            <Button path='goBack'>Go Back</Button>
-            <Button path="/home">Go Home</Button>
+            <div className={Styles.container}>
+                <p className={Styles.title}>Oops!</p>
+                <div className={Styles.content}>
+                <p className={Styles.subtitle}>404 - PAGE NOT FOUND</p>
+                <p>Sorry, we couldn't find {location.pathname}. The page that you looking for might have been removed had it's name changed or temprorarily unavailable</p>
+                </div>
+                <div className={Styles.buttons}>
+                    <Button shape='round' path='goBack'>Go Back</Button>
+                    <Button shape='round' fill='outline' path="/home">Go Home</Button>
+                </div>
+            </div>
         </IonPage>
     );
 };
