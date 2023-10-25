b25f81eee6577f70dcfc2dadb9857bdfdac046d9
nadyaps <sadinp21d@student.unhas.ac.id>
2023-10-25 15:13:47 +0800

    adding product page

---
+++ src/pages/product.tsx
@@ -0,0 +1,60 @@
+import Styles from '../styles/Artikel.module.css';
+import { IonContent, IonPage, IonHeader, IonTitle, IonCard, IonCardHeader, IonCardTitle, IonCardContent, IonCardSubtitle, IonThumbnail, IonImg } from '@ionic/react';
+import Navbar from '../components/organisms/Navbar';
+import Footer from '../components/organisms/Footer';
+import Pic from '../../assets/images/istockphoto-91633822-612x612.jpg'
+
+const Artikel = () => {
+
+    return (
+        <IonPage>
+            <Navbar>Berita dan Pengumuman</Navbar>
+            <IonContent>
+                <div className={Styles.container}>
+                    {/* for the title of the page */}
+                    <div className={Styles.title}>
+                        <h1>Berita dan Pengumuman</h1>
+                    </div>
+                    <div className={Styles.responsive}>
+                        <IonCard>
+                            <IonImg src={Pic} />
+                            <IonCardHeader>
+                                <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                                <IonCardTitle>Card Title</IonCardTitle>
+                            </IonCardHeader>
+                            <IonCardContent>
+                                Keep close to Nature's heart... and break clear away, once in awhile,
+                                and climb a mountain or spend a week in the woods. Wash your spirit clean.
+                            </IonCardContent>
+                        </IonCard>
+                        <IonCard>
+                            <IonImg src={Pic} />
+                            <IonCardHeader>
+                                <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                                <IonCardTitle>Card Title</IonCardTitle>
+                            </IonCardHeader>
+                            <IonCardContent>
+                                Keep close to Nature's heart... and break clear away, once in awhile,
+                                and climb a mountain or spend a week in the woods. Wash your spirit clean.
+                            </IonCardContent>
+                        </IonCard>
+                        <IonCard>
+                            <IonImg src={Pic} />
+                            <IonCardHeader>
+                                <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                                <IonCardTitle>Card Title</IonCardTitle>
+                            </IonCardHeader>
+                            <IonCardContent>
+                                Keep close to Nature's heart... and break clear away, once in awhile,
+                                and climb a mountain or spend a week in the woods. Wash your spirit clean.
+                            </IonCardContent>
+                          </IonCard>
+                          </div>
+                </div>
+            </IonContent>
+            <Footer />
+        </IonPage>
+    )
+}
+
+export default Artikel;
