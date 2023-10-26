fb0f67d73b1ba960339f7a4ea2ebc1044a9b54ba
syarifahb <syarifahb04@gmail.com>
2023-10-26 20:56:03 +0800

    update page

--- src/pages/Landing.tsx
+++ src/pages/Landing.tsx
@@ -1,4 +1,4 @@
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonMenu, IonMenuButton, IonButton, IonButtons, IonText } from '@ionic/react';
+import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar, IonMenu, IonMenuButton, IonButton, IonImg, IonText, IonCard, IonCardHeader, IonCardSubtitle, IonCardContent, IonCardTitle } from '@ionic/react';
 import Footer from '../components/organisms/Footer';
 import './Landing.css';
 import Navbar from '../components/organisms/Navbar';
@@ -14,11 +14,64 @@ const Landing: React.FC = () => {
            <IonText><h1 className='landing-content-heading1'>Welcome to My Landing Page</h1></IonText>
             <IonText><p className='landing-content-heading2'>This is a simple landing page.</p></IonText>
             <IonButton> 
-              Start 
+              Get Started!
             </IonButton>
             </section>
             <img src='./Fontaine Genshin impact.jpg' alt="image"/>
           </div>
+          
+
+          <IonText className='card_text'> Berita Terkini </IonText> 
+          <div className="card_container">
+            <div className="card_title">
+             <div className="card_responsive" >
+                <IonCard>
+                  <IonImg />
+                    <IonCardHeader>
+                        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                        <IonCardTitle>Card Title</IonCardTitle>
+                        </IonCardHeader>
+                        <IonCardContent>
+                            Keep close to Nature's heart... and break clear away, once in awhile,
+                            and climb a mountain or spend a week in the woods. Wash your spirit clean.
+                        </IonCardContent>
+                  </IonCard>
+                  <IonCard>
+                  <IonImg />
+                    <IonCardHeader>
+                        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                        <IonCardTitle>Card Title</IonCardTitle>
+                        </IonCardHeader>
+                        <IonCardContent>
+                            Keep close to Nature's heart... and break clear away, once in awhile,
+                            and climb a mountain or spend a week in the woods. Wash your spirit clean.
+                        </IonCardContent>
+                  </IonCard>
+                  <IonCard>
+                  <IonImg />
+                    <IonCardHeader>
+                        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                        <IonCardTitle>Card Title</IonCardTitle>
+                        </IonCardHeader>
+                        <IonCardContent>
+                            Keep close to Nature's heart... and break clear away, once in awhile,
+                            and climb a mountain or spend a week in the woods. Wash your spirit clean.
+                        </IonCardContent>
+                  </IonCard>
+                  <IonCard>
+                  <IonImg />
+                    <IonCardHeader>
+                        <IonCardSubtitle>Card Subtitle</IonCardSubtitle>
+                        <IonCardTitle>Card Title</IonCardTitle>
+                        </IonCardHeader>
+                        <IonCardContent>
+                            Keep close to Nature's heart... and break clear away, once in awhile,
+                            and climb a mountain or spend a week in the woods. Wash your spirit clean.
+                        </IonCardContent>
+                  </IonCard>
+              </div>
+            </div>
+          </div>  
         </IonContent>
         <Footer />
     </IonPage>
