63580fefd50d2f5f1a8a19124d7e5d7fc80f3cb3
syarifahb <syarifahb04@gmail.com>
2023-10-26 20:57:08 +0800

    update

--- src/pages/About.tsx
+++ src/pages/About.tsx
@@ -1,6 +1,5 @@
 import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
 import Footer from '../components/organisms/Footer';
-import Header from '../components/organisms/Header';
 import './About.css';
 import Navbar from '../components/organisms/Navbar';
 
