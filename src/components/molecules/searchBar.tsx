5754cef43e45ef9ec52e1482754e529187d4ea9b
FrhnSpwli <96256195+FrhnSpwli@users.noreply.github.com>
2023-11-01 21:25:53 +0800

    fix searchbar props

--- src/components/molecules/searchBar.tsx
+++ src/components/molecules/searchBar.tsx
@@ -1,17 +1,26 @@
-import { ReactNode } from 'react';
+import React, { ReactNode } from 'react';
 import { IonSearchbar } from '@ionic/react';
 import Styles from './searchBar.module.css';
 
 interface SearchBarProps {
     children: ReactNode;
+    onIonInput: (event: CustomEvent<void>) => void;
 }
 
-const SearchBar = ({ children }: SearchBarProps) => {
-
+const SearchBar = ({ children, onIonInput }: SearchBarProps) => {
     return (
         <div className={Styles.searchBar}>
-            <IonSearchbar placeholder={`${children}`} mode='ios' animated={true} color={"light"} spellCheck={'true'}></IonSearchbar>
+            <IonSearchbar
+                debounce={1000}
+                placeholder={`${children}`}
+                mode='ios'
+                animated={true}
+                color="light"
+                spellCheck={true}
+                onIonInput={onIonInput}
+            ></IonSearchbar>
         </div>
     );
 }
+
 export default SearchBar;
