b0e56d08b7a4c59776a6328c5979c5e5e890d574
JetAriel <arielnoahswag@gmail.com>
2023-11-19 21:22:47 +0800

    update table

--- src/pages/AdministrasiPenduduk.tsx
+++ src/pages/AdministrasiPenduduk.tsx
@@ -1,16 +1,18 @@
-import { useMemo } from 'react';
+import { useMemo } from "react";
 import {
   MaterialReactTable,
   useMaterialReactTable,
   type MRT_ColumnDef,
-} from 'material-react-table';
+} from "material-react-table";
 import Footer from "../components/organisms/Footer";
 import Navbar from "../components/organisms/Navbar";
-import { IonContent, IonPage } from '@ionic/react';
-import Styles from '../styles/AdministrasiPenduduk.module.css'
+import { IonContent, IonPage } from "@ionic/react";
+import Styles from "../styles/AdministrasiPenduduk.module.css";
+import { IntegerType } from "mongodb";
 
 //example data type
 type Person = {
+  nomor: string;
   name: {
     firstName: string;
     lastName: string;
@@ -24,54 +26,59 @@ type Person = {
 //nested data is ok, see accessorKeys in ColumnDef below
 const data: Person[] = [
   {
+    nomor: "1",
     name: {
-      firstName: 'Ayaka',
-      lastName: 'Kamisato',
+      firstName: "Ayaka",
+      lastName: "Kamisato",
     },
-    address: 'Inazuma',
-    email: 'ayaka@gmail.com',
-    gender: 'Woman',
-    religion: 'Christian',
+    address: "Inazuma",
+    email: "ayaka@gmail.com",
+    gender: "Woman",
+    religion: "Christian",
   },
   {
+    nomor: "2",
     name: {
-      firstName: 'Kazuha',
-      lastName: 'Kaedehara',
+      firstName: "Kazuha",
+      lastName: "Kaedehara",
     },
-    address: 'Inazuma',
-    email: 'kazuha@gmail.com',
-    gender: 'Man',
-    religion: 'Christian',
+    address: "Inazuma",
+    email: "kazuha@gmail.com",
+    gender: "Man",
+    religion: "Christian",
   },
   {
+    nomor: "3",
     name: {
-      firstName: 'Sara',
-      lastName: 'Kujou',
+      firstName: "Sara",
+      lastName: "Kujou",
     },
-    address: 'Inazuma',
-    email: 'sara@gmail.com',
-    gender: 'Woman',
-    religion: 'Christian',
+    address: "Inazuma",
+    email: "sara@gmail.com",
+    gender: "Woman",
+    religion: "Christian",
   },
   {
+    nomor: "4",
     name: {
-      firstName: 'Shogun',
-      lastName: 'Raiden',
+      firstName: "Shogun",
+      lastName: "Raiden",
     },
-    address: 'Inazuma',
-    email: 'Raiden@gmail.com',
-    gender: 'Woman',
-    religion: 'Christian',
+    address: "Inazuma",
+    email: "Raiden@gmail.com",
+    gender: "Woman",
+    religion: "Christian",
   },
   {
+    nomor: "5",
     name: {
-      firstName: 'Kokomi',
-      lastName: 'Sangonomia',
+      firstName: "Kokomi",
+      lastName: "Sangonomia",
     },
-    address: 'Inazuma',
-    email: 'kokomi@gmail.com',
-    gender: 'Woman',
-    religion: 'Christian',
+    address: "Inazuma",
+    email: "kokomi@gmail.com",
+    gender: "Woman",
+    religion: "Christian",
   },
 ];
 
@@ -80,37 +87,42 @@ const Example = () => {
   const columns = useMemo<MRT_ColumnDef<Person>[]>(
     () => [
       {
-        accessorKey: 'name.firstName', //access nested data with dot notation
-        header: 'First Name',
+        accessorKey: "nomor",
+        header: "No",
         size: 150,
       },
       {
-        accessorKey: 'name.lastName',
-        header: 'Last Name',
+        accessorKey: "name.firstName", //access nested data with dot notation
+        header: "First Name",
         size: 150,
       },
       {
-        accessorKey: 'address', //normal accessorKey
-        header: 'Address',
+        accessorKey: "name.lastName",
+        header: "Last Name",
+        size: 150,
+      },
+      {
+        accessorKey: "address", //normal accessorKey
+        header: "Address",
         size: 200,
       },
       {
-        accessorKey: 'email',
-        header: 'Email',
+        accessorKey: "email",
+        header: "Email",
         size: 150,
       },
       {
-        accessorKey: 'gender',
-        header: 'Gender',
+        accessorKey: "gender",
+        header: "Gender",
         size: 150,
       },
       {
-        accessorKey: 'religion',
-        header: 'Religion',
+        accessorKey: "religion",
+        header: "Religion",
         size: 150,
       },
     ],
-    [],
+    []
   );
 
   const table = useMaterialReactTable({
@@ -120,16 +132,15 @@ const Example = () => {
 
   return (
     <IonPage>
-        <Navbar>Administrasi Penduduk</Navbar>
-        <IonContent>
-            <div className={Styles.container}>
-            <MaterialReactTable table={table} />
-            </div>
-        </IonContent>
-        <Footer />
+      <Navbar>Administrasi Penduduk</Navbar>
+      <IonContent>
+        <div className={Styles.container}>
+          <MaterialReactTable table={table} />
+        </div>
+      </IonContent>
+      <Footer />
     </IonPage>
-   
-  )
+  );
 };
 
 export default Example;
